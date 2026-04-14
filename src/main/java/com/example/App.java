2aa1bc473d84ffedbeddcfc71979f048ea8e9624
TATIKONDASOWMYA <tatikondasowmya2005@gmail.com>
2026-04-14 21:23:15 +0530

    Second Commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,30 +2,59 @@ package com.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Required for Jenkins (headless Linux)
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://automationexercise.com");
 
         driver.manage().window().maximize();
 
-        Thread.sleep(4000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        // Click first "Add to Cart" button
-        driver.findElement(By.xpath("(//a[contains(text(),'Add to cart')])[1]")).click();
+        try {
+            // Wait until "Add to cart" is clickable
+            WebElement addToCart = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                    By.xpath("(//a[contains(text(),'Add to cart')])[1]")
+                )
+            );
 
-        Thread.sleep(4000);
+            addToCart.click();
 
-        // Click Continue Shopping
-        driver.findElement(By.xpath("//button[contains(text(),'Continue Shopping')]")).click();
+            // Wait for Continue Shopping button
+            WebElement continueBtn = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                    By.xpath("//button[contains(text(),'Continue Shopping')]")
+                )
+            );
 
-        System.out.println("First item added to cart successfully");
+            continueBtn.click();
 
-        driver.quit();
+            System.out.println("First item added to cart successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
